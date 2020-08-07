7727ea3f5217c5df6b3bf4ce19cdad04fbbdaa34
ugaliguy <ugali_guy@hotmail.com>
2020-08-06 19:52:39 -0500

    Changed to iterative Fibonacci method

--- Algorithmic-Toolbox/week-2/Fibonacci.java
+++ Algorithmic-Toolbox/week-2/Fibonacci.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 
 public class Fibonacci {
   private static long calc_fib(int n) {
-    if (n <= 1)
-      return n;
-
-    return calc_fib(n - 1) + calc_fib(n - 2);
+//    if (n <= 1)
+//      return n;
+//
+//    return calc_fib(n - 1) + calc_fib(n - 2);
+	  int[] fib = new int[n+1];
+	  fib[0] = 0;
+	  fib[1] = 1;
+	  for (int i = 2; i <= n; i++) {
+		  fib[i] = fib[i-1] + fib[i-2];
+	  }
+	  
+	  return fib[n];
   }
 
   public static void main(String args[]) {
@@ -13,5 +21,7 @@ public class Fibonacci {
     int n = in.nextInt();
 
     System.out.println(calc_fib(n));
+    
+    in.close();
   }
 }
