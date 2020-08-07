263b1bbb877da2c5742f0c749a8539b64a1d9336
ugaliguy <ugali_guy@hotmail.com>
2020-08-07 09:38:50 -0500

    Replace slow algorithm with faster method

--- Algorithmic-Toolbox/week-2/LCM.java
+++ Algorithmic-Toolbox/week-2/LCM.java
@@ -1,19 +1,33 @@
 import java.util.*;
 
 public class LCM {
-  private static long lcm_naive(int a, int b) {
-    for (long l = 1; l <= (long) a * b; ++l)
-      if (l % a == 0 && l % b == 0)
-        return l;
+//  private static long lcm_naive(int a, int b) {
+//    for (long l = 1; l <= (long) a * b; ++l)
+//      if (l % a == 0 && l % b == 0)
+//        return l;
+//
+//    return (long) a * b;
+//  }
+//  
+  private static int euclid(int a, int b) {
+	    if (b == 0) {
+	    	return a;
+	    }
+	    int cd = a%b;
 
-    return (long) a * b;
-  }
+	    return euclid(b,cd);
+	  }
+  
+  private static long lcm_efficient(int a, int b) {
+	    return (long) a*b/euclid(a,b);
+	  }
 
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
 
-    System.out.println(lcm_naive(a, b));
+    System.out.println(lcm_efficient(a, b));
+    scanner.close();
   }
 }
