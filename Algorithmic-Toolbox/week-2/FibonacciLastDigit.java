b938399efaa75ca464ff57da9adf0a23c60a05a4
ugaliguy <ugali_guy@hotmail.com>
2020-08-06 21:16:11 -0500

    Changed method to faster algorithm

--- Algorithmic-Toolbox/week-2/FibonacciLastDigit.java
+++ Algorithmic-Toolbox/week-2/FibonacciLastDigit.java
@@ -2,19 +2,26 @@ import java.util.*;
 
 public class FibonacciLastDigit {
     private static int getFibonacciLastDigitNaive(int n) {
-        if (n <= 1)
-            return n;
-
-        int previous = 0;
-        int current  = 1;
-
-        for (int i = 0; i < n - 1; ++i) {
-            int tmp_previous = previous;
-            previous = current;
-            current = tmp_previous + current;
-        }
-
-        return current % 10;
+//        if (n <= 1)
+//            return n;
+//
+//        int previous = 0;
+//        int current  = 1;
+//
+//        for (int i = 0; i < n - 1; ++i) {
+//            int tmp_previous = previous;
+//            previous = current;
+//            current = tmp_previous + current;
+//        }
+//
+//        return current % 10;
+    	int[] fib = new int[n+1];
+    	fib[0] = 0;
+    	fib[1] = 1;
+    	for (int i = 2; i <= n; i++) {
+    		fib[i] = (fib[i-1] + fib[i-2]) % 10;
+    	}
+    	return fib[n];
     }
     
     public static void main(String[] args) {
@@ -22,6 +29,7 @@ public class FibonacciLastDigit {
         int n = scanner.nextInt();
         int c = getFibonacciLastDigitNaive(n);
         System.out.println(c);
+        scanner.close();
     }
 }
 
