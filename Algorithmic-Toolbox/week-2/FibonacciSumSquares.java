0c4bd2e4e895df1dd53a358fe4a94935d0212bde
ugaliguy <ugali_guy@hotmail.com>
2020-08-10 10:28:20 -0500

    Add methods for Pisano period and array of last digits of Fibonacci numbers

--- Algorithmic-Toolbox/week-2/FibonacciSumSquares.java
+++ Algorithmic-Toolbox/week-2/FibonacciSumSquares.java
@@ -1,9 +1,63 @@
 import java.util.*;
 
 public class FibonacciSumSquares {
-    private static long getFibonacciSumSquaresNaive(long n) {
-        if (n <= 1)
+//    private static long getFibonacciSumSquaresNaive(long n) {
+//        if (n <= 1)
+//            return n;
+//
+//        long previous = 0;
+//        long current  = 1;
+//        long sum      = 1;
+//
+//        for (long i = 0; i < n - 1; ++i) {
+//            long tmp_previous = previous;
+//            previous = current;
+//            current = tmp_previous + current;
+//            sum += current * current;
+//        }
+//
+//        return sum % 10;
+//    }
+	private static int pisanoPeriod(long m) {
+		long prev = 0;
+		long curr = 1;
+		long next = 0;
+		int count = 0;
+		for (int i = 0; i < m*m; i++) {
+			next = (prev + curr) % m;
+			prev = curr;
+			curr = next;
+			if (prev == 0 && curr == 1) {
+				count = i + 1;
+				break;
+			}
+		}
+		return count;
+	}
+	
+	private static ArrayList<Integer> pisano(int m) {
+		int p = pisanoPeriod(m);
+		ArrayList<Integer> remainders = new ArrayList<Integer>();
+		int i = 2;
+		remainders.add(0);
+		remainders.add(1);
+		do {
+			remainders.add((remainders.get(i-1) + remainders.get(i-2)) % m);
+			i += 1;
+		} while (i < p);
+//		for (Integer d: remainders) {
+//			System.out.print(d);
+//		}
+//		System.out.println(" ");
+		return remainders;
+	}
+	
+	private static long getFibonacciSumSquaresEfficient(long n) {
+		if (n <= 1)
             return n;
+		
+		ArrayList<Integer> ones = pisano(10);
+		int p = pisanoPeriod(10);
 
         long previous = 0;
         long current  = 1;
@@ -22,7 +76,7 @@ public class FibonacciSumSquares {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumSquaresNaive(n);
+        long s = getFibonacciSumSquaresEfficient(n);
         System.out.println(s);
     }
 }
