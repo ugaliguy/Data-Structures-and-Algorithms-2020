41b85317c8ed3fd208a8123fc67498787e8de262
ugaliguy <ugali_guy@hotmail.com>
2020-08-12 21:19:45 -0500

    Implemented efficient algorithm - NEEDS DEBUGGING!

--- Algorithmic-Toolbox/week-3-Greedy-Algorithms/CarFueling.java
+++ Algorithmic-Toolbox/week-3-Greedy-Algorithms/CarFueling.java
@@ -3,7 +3,27 @@ import java.io.*;
 
 public class CarFueling {
     static int computeMinRefills(int dist, int tank, int[] stops) {
-        return -1;
+    	int refills = 0;
+    	int current = 0;
+    	int last = 0;
+    	
+    	while (current < (dist - 1)) {
+    		last = current;
+    		System.out.println(current + 1);
+    		while ((current < stops.length - 1) && (stops[current + 1] - stops[last] <= tank)) {
+    			current += 1;
+    			if (current == dist - 1) {
+    				break;
+    			}
+    		}
+    		if (current == last) {
+    			return -1;
+    		}
+    		if (current < last - 1) {
+    			refills += 1;
+    		}
+    	}
+        return refills;
     }
 
     public static void main(String[] args) {
@@ -17,5 +37,6 @@ public class CarFueling {
         }
 
         System.out.println(computeMinRefills(dist, tank, stops));
+        scanner.close();
     }
 }
