6e074928ab8c3c32455531dd872987fd85ceec60
ugaliguy <ugali_guy@hotmail.com>
2020-08-16 16:06:18 -0500

    Added algorithm to return an ArrayList of points

--- Algorithmic-Toolbox/week-3-Greedy-Algorithms/CoveringSegments.java
+++ Algorithmic-Toolbox/week-3-Greedy-Algorithms/CoveringSegments.java
@@ -2,24 +2,35 @@ import java.util.*;
 
 public class CoveringSegments {
 
-    private static int[] optimalPoints(Segment[] segments) {
+    private static ArrayList<Integer> optimalPoints(Segment[] segments) {
         //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
+
+    	Comparator<Segment> a = (X, Y) -> X.end > Y.end ? 1 : -1;
+        Arrays.sort(segments, a);
+
+        ArrayList<Integer> points = new ArrayList<>();
+        int point = segments[0].end;
+        points.add(point);
+        for (int i = 1; i < segments.length; i++) {
+            if (point < segments[i].start) {
+                point = segments[i].end;
+                points.add(point);
+            }
         }
+
         return points;
     }
 
     private static class Segment {
-        int start, end;
+    	int start, end;
 
         Segment(int start, int end) {
             this.start = start;
             this.end = end;
         }
     }
+        
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -30,11 +41,13 @@ public class CoveringSegments {
             end = scanner.nextInt();
             segments[i] = new Segment(start, end);
         }
-        int[] points = optimalPoints(segments);
-        System.out.println(points.length);
+        ArrayList<Integer> points = optimalPoints(segments);
+        System.out.println(points.size());
         for (int point : points) {
             System.out.print(point + " ");
         }
+        
+        scanner.close();
     }
 }
  
