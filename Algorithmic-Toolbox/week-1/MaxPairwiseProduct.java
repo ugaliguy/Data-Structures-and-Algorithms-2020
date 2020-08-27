c07e2eeb3f626c42f23060bbb2d336d0f45a8e6b
ugaliguy <ugali_guy@hotmail.com>
2020-08-27 12:16:20 -0500

    Need to fix bug - wrong answer for reversed array

--- Algorithmic-Toolbox/week-1/MaxPairwiseProduct.java
+++ Algorithmic-Toolbox/week-1/MaxPairwiseProduct.java
@@ -2,10 +2,50 @@ import java.util.*;
 import java.io.*;
 
 public class MaxPairwiseProduct {
-    static int getMaxPairwiseProduct(int[] numbers) {
+    static long getMaxPairwiseProduct(int[] numbers) {
         int n = numbers.length;
-        Arrays.sort(numbers);
-        return numbers[n-1]*numbers[n-2];
+        int i0 = 0;
+//        int i1 = 1;
+        for (int i = 1; i < n; i++) {
+        	if (numbers[i] > numbers[i0]) {
+        		i0 = i;
+//        		System.out.print("first = ");
+//                System.out.println(i);
+        	}
+        }
+        // Swap numbers[i0] with numbers[n-1]
+        int dummy0 = numbers[n-1];
+        numbers[n-1] = numbers[i0];
+        numbers[i0] = dummy0;
+        
+//        System.out.print("i0 = ");
+//        System.out.println(i0);
+        
+//        if (i0 != 0) {
+//        	i1 = 0;
+//        }
+//        for (int i = 0; i < n; i++) {
+//        	if (numbers[i] != numbers[i0] && numbers[i] > numbers[i1]) {
+//        		i1 = i;
+//        		System.out.print("second = ");
+//                System.out.println(i);
+//        	}
+//        }
+//        System.out.print("i1 = ");
+//        System.out.println(i1);
+        i0 = 0;
+        for (int i = 1; i < n-1; i++) {
+        	if (numbers[i] > numbers[i0]) {
+        		i0 = i;
+//      		System.out.print("first = ");
+//              System.out.println(i);
+        	}
+      }
+      // Swap numbers[i0] with numbers[n-1]
+      int dummy1 = numbers[n-2];
+      numbers[n-1] = numbers[i0];
+      numbers[i0] = dummy1;
+        return (long) numbers[n-1]*numbers[n-2];
     }
 
     public static void main(String[] args) {
