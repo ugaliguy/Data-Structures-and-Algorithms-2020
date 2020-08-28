836688cd394184a3697c0778fb2c8d263490a70b
ugaliguy <ugali_guy@hotmail.com>
2020-08-28 10:54:08 -0500

    Need to fix some Off By 1 bugs

--- Algorithmic-Toolbox/week-2/FibonacciHuge.java
+++ Algorithmic-Toolbox/week-2/FibonacciHuge.java
@@ -17,39 +17,57 @@ public class FibonacciHuge {
 //        return current % m;
 //    }
     
-	private static long pisanoPeriod(long m) {
-		long prev = 0;
-		long curr = 1;
-		long next = 0;
-		long count = 0;
-		for (long i = 0; i < m*m; i++) {
-			next = (prev + curr) % m;
-			prev = curr;
-			curr = next;
-			if (prev == 0 && curr == 1) {
-				count = i + 1;
+	private static ArrayList<Long> pisanoPeriod(long m) {
+//		long prev = 0;
+//		long curr = 1;
+//		long next = 0;
+		long result = 1;
+		ArrayList<Long> pisano = new ArrayList<Long>();
+		pisano.add(0L);
+		pisano.add(1L);
+		int i = 2;
+		while(true) {
+			pisano.add((pisano.get(i-1) + pisano.get(i-2))%m);
+			i += 1;
+			// Check for subsequence 01
+			if (pisano.get(i-2) == 0 && pisano.get(i-1) == 1) {
 				break;
 			}
 		}
-		return count;
+//		System.out.println(Arrays.toString(pisano.toArray()));
+//		System.out.println(pisano.size());
+//		for (long i = 0; i < m*m; i++) {
+//			next = (prev + curr) % m;
+//			prev = curr;
+//			curr = next;
+//			if (prev == 0 && curr == 1) {
+//				count = i + 1;
+//				break;
+//			}
+//		}
+		return pisano;
 	}
 	
     private static long getFibonacciHugeEfficient(long n, long m) {
-        long p = pisanoPeriod(m);
+        ArrayList<Long> p = pisanoPeriod(m);
+        int period = p.size() - 2;
 //        System.out.println(p);
-        n = n % p;
-        if (m <= 1) {
-        	return m;
-        }
-        long prev = 0;
-        long curr = 1;
-        long next = 0;
-        for (long i = 0; i < n-1; i++) {
-        	next = (prev + curr) % m;
-        	prev = curr;
-        	curr = next;
-        }
-        return next % m;
+        long remainder = n % period;
+//        System.out.println(n);
+//        if (m <= 1) {
+//        	return m;
+//        }
+//        long prev = 0;
+//        long curr = 1;
+//        long next = 1;
+//        for (long i = 0; i < n-1; i++) {
+//        	next = (prev + curr) % m;
+//        	prev = curr;
+//        	curr = next;
+////        	System.out.println(next);
+//        }
+        
+        return remainder;
     }
     
     public static void main(String[] args) {
