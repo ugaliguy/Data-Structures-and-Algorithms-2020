2c3a566bdf62bd2f8e63ff9b3bba6d02de2250d4
ugaliguy <ugali_guy@hotmail.com>
2020-08-28 16:42:22 -0500

    Modified getFibonacciSumEfficient to be more efficient

--- Algorithmic-Toolbox/week-2/FibonacciSumLastDigit.java
+++ Algorithmic-Toolbox/week-2/FibonacciSumLastDigit.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class FibonacciSumLastDigit {
-    
+	
+	// Take advantage of the identity F_0 + F_1 + F_2 + ... +F_n = F_(n+2) - 1
+	
 	private static int pisanoPeriod(long m) {
 		long prev = 0;
 		long curr = 1;
@@ -19,12 +21,12 @@ public class FibonacciSumLastDigit {
 		return count;
 	}
 	
-	private static ArrayList<Integer> pisano(int m) {
+	private static ArrayList<Long> pisano(int m) {
 		int p = pisanoPeriod(m);
-		ArrayList<Integer> remainders = new ArrayList<Integer>();
+		ArrayList<Long> remainders = new ArrayList<Long>();
 		int i = 2;
-		remainders.add(0);
-		remainders.add(1);
+		remainders.add(0L);
+		remainders.add(1L);
 		do {
 			remainders.add((remainders.get(i-1) + remainders.get(i-2)) % m);
 			i += 1;
@@ -33,23 +35,28 @@ public class FibonacciSumLastDigit {
 	}
 	
     private static long getFibonacciSumEfficient(long n) {
-    	ArrayList<Integer> lastDigits = pisano(10);
-    	
-        if (n <= 1)
-            return n;
-
-        long previous = 0;
-        long current  = 1;
-        long sum      = 1;
-
-        for (long i = 0; i < n - 1; ++i) {
-            long tmp_previous = previous;
-            previous = current % 10;
-            current = (tmp_previous + current) % 10;
-            sum = (sum + current) % 10;
-        }
-
-        return sum % 10;
+//    	ArrayList<Integer> lastDigits = pisano(10);
+//    	
+//        if (n <= 1)
+//            return n;
+//
+//        long previous = 0;
+//        long current  = 1;
+//        long sum      = 1;
+//
+//        for (long i = 0; i < n - 1; ++i) {
+//            long tmp_previous = previous;
+//            previous = current % 10;
+//            current = (tmp_previous + current) % 10;
+//            sum = (sum + current) % 10;
+//        }
+//
+//        return sum % 10;
+    	ArrayList<Long> p = pisano(10);
+        int period = p.size() - 2;
+//        System.out.println(p);
+        Long remainder = n+2 % period;
+        return p.get(remainder.intValue()) - 1;
     }
     
     public static void main(String[] args) {
