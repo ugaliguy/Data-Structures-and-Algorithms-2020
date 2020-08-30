d4a49e54ae90c8c4d803221c47948e3d693302a2
ugaliguy <ugali_guy@hotmail.com>
2020-08-29 22:01:56 -0500

    Fixed an edge case bug

--- Algorithmic-Toolbox/week-3-Greedy-Algorithms/FractionalKnapsack.java
+++ Algorithmic-Toolbox/week-3-Greedy-Algorithms/FractionalKnapsack.java
@@ -4,50 +4,16 @@ import java.util.Arrays;
 
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
-//    	int n = values.length;
-//    	double[][] ratios = new double[n][3];
-//    	for (int i = 0; i < n; i++) {
-//    		ratios[i][0] = values[i];
-//    		ratios[i][1] = weights[i];
-//    		ratios[i][2] = 1.0*values[i]/weights[i];
-//    	}
-////    	System.out.println(ratios.length);
-////    	Comparator<double[]> c = (X, Y) -> X[0] / X[1] < Y[0] / Y[1] ? 1 : -1;
-//    	Comparator<double[]> c = (X, Y) -> X[2] > Y[2] ? 1 : -1;
-//    	Arrays.sort(ratios, c);
-//        double result = 0;
-//        int j = 0;
-////        while (capacity > 0 && j < n) {
-////        	double a = Math.min(ratios[0][1], capacity);
-////        	result += a*ratios[j][0]/ratios[j][1];
-////        	capacity -= a;
-////        	j += 1;
-////        }
-////        return result;
-////        int index= ratios.length-1;
-//        while(capacity > 0 && j < n){
-//            double[] item = ratios[n-1];
-//            if (capacity >= item[1]){
-//                capacity-=item[1];
-//                result += item[0];
-//            }else{
-//
-//                result += capacity*item[2];
-//                capacity = 0;
-//            }
-//            j += 1;
-//        }
-//
-//        return (double)Math.round(result*10000d)/10000d;
     	double value = 0;
-        //we sort the items by its value per unit
-        double items[][]=new double[values.length][3];
-        for (int i=0; i<values.length; i++){
-            items[i][0]=(double)values[i]/weights[i];
-            items[i][1]=(double)values[i];
-            items[i][2]=(double)weights[i];
+    	int n = values.length;
+        double items[][]=new double[n][3];
+        for (int i = 0; i < n; i++){
+            items[i][0] = (double) values[i]/weights[i];
+            items[i][1] = (double) values[i];
+            items[i][2] = (double) weights[i];
         }
-
+        
+        // Sort the items by its value per unit
         Arrays.sort(items, new Comparator<double[]>() {
             @Override
             public int compare(double[] o1, double[] o2) {
@@ -55,20 +21,18 @@ public class FractionalKnapsack {
             }
         });
 
-
-
-        int index=items.length-1;
-        while(capacity>0 && index>=0){
+        int index = items.length - 1;
+        while(capacity > 0 && index >= 0){
             double[] item=items[index];
-            if (capacity>=item[2]){
-                capacity-=item[2];
-                value+=item[1];
-            }else{
+            if (capacity >= item[2]) {
+                capacity -= item[2];
+                value += item[1];
+            }else {
 
                 value+= capacity*item[0];
                 capacity=0;
             }
-            index--;
+            index -= 1;
         }
 
         return (double)Math.round(value * 10000d) / 10000d;
@@ -85,5 +49,6 @@ public class FractionalKnapsack {
             weights[i] = scanner.nextInt();
         }
         System.out.println(getOptimalValue(capacity, values, weights));
+        scanner.close();
     }
 } 
