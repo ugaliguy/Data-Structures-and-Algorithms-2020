dd93c0a6e66d38b5e0a15918f59c736720c52a32
ugaliguy <ugali_guy@hotmail.com>
2020-08-31 14:55:56 -0500

    Deleted comments and added variables for array lengths

--- Algorithmic-Toolbox/week-4-Divide-and-Conquer/PointsAndSegments.java
+++ Algorithmic-Toolbox/week-4-Divide-and-Conquer/PointsAndSegments.java
@@ -21,85 +21,18 @@ public class PointsAndSegments {
     }
 
     private static int[] fastCountSegments(int[] starts, int[] ends, int[] points) {
-//        int[] cnt = new int[points.length];
-//        int n = starts.length;
-//        int p = points.length;
-//        int i = 0;
-//        Pair[] pairs = new Pair[n*2 + p];
-//        HashMap<Integer,ArrayList<Integer>> pointsMap = new HashMap<Integer,ArrayList<Integer>>();
-//        
-//        System.out.println(Arrays.toString(starts));
-//        System.out.println(Arrays.toString(ends));
-//        //write your code here
-//        //Label each point as follows starts:1, ends:3 and points:2
-//        for(i = 0; i < n; i++)
-////        	System.out.println(start);
-//            pairs[i] = new Pair(starts[i],1);
-//        	System.out.print("start i = ");
-//        	System.out.print(i);
-//        	System.out.print(" start =  ");
-//        	System.out.println(pairs[i]);
-//        	i += 1;
-//        	
-//        for(i = 0; i < n; i++)
-//            pairs[i] = new Pair(ends[i],3);
-//        	System.out.print("end i = ");
-//        	System.out.print(i);
-//        	System.out.print(" end =  ");
-//        	System.out.println(pairs[i]);
-//            i += 1;
-//            
-//        for(int j=0; j < p; j++) {
-//        	pairs[i]=new Pair(points[j],2);
-//        	i += 1;
-//        	if (pointsMap.containsKey(points[j])) {
-//        		pointsMap.get(points[j]).add(j);
-//        	}
-//        	else {
-//        		ArrayList<Integer> list= new ArrayList<>();
-//                list.add(j);
-//                pointsMap.put(points[j],list);
-//        	}
-//        }
-//        
-//        for (Pair pair: pairs) {
-////        	System.out.print("FARTZ: ");
-////        	System.out.println(pair.toString());
-//        	System.out.println(pair);
-//        }
-//        
-//        Arrays.sort(pairs, new Comparator<Pair>() {
-//            @Override
-//            public int compare(Pair o1, Pair o2) {
-//                if(o1.a==o2.a)
-//                    return Integer.compare(o1.b, o2.b);
-//                return Integer.compare(o1.a, o2.a);
-//            }
-//        });
-//        
-//        int coverage=0;
-//        for (Pair pair: pairs){
-//            if(pair.b==1) //If it's a starting point of any segment
-//                coverage++;
-//            else if(pair.b==3) //If it's a ending point of any segment
-//                coverage--;
-//            else { //if it's a point
-//                ArrayList<Integer> indexes=pointsMap.get(pair.a);
-//                for(Integer k:indexes){
-//                    cnt[k]=coverage;
-//                }
-//            }
-//        }
-//        return cnt;
-    	int[] cnt = new int[points.length];
+    	int s = starts.length;
+    	int e = ends.length;
+    	int p = points.length;
+    	int[] cnt = new int[p];
         Arrays.sort(starts);
         Arrays.sort(ends);
-        for (int i = 0; i < points.length; i++) {
+        for (int i = 0; i < p; i++) {
             int si = 0, ei = 0;
 
-            while (si < starts.length && starts[si] <= points[i]) si++;
+            while (si < s && starts[si] <= points[i]) si++;
 
-            while (ei < ends.length && ends[ei] < points[i]) ei++;
+            while (ei < e && ends[ei] < points[i]) ei++;
 
             cnt[i] = si - ei;
         }
