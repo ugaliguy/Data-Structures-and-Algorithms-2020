9960312318d25818e6e8d4ec5317f75dd3a38e7e
ugaliguy <ugali_guy@hotmail.com>
2020-09-03 16:28:12 -0500

    Added code for lcs2 method

--- Algorithmic-Toolbox/week-5-Dynamic-Programming-1/LCS2.java
+++ Algorithmic-Toolbox/week-5-Dynamic-Programming-1/LCS2.java
@@ -4,7 +4,18 @@ public class LCS2 {
 
     private static int lcs2(int[] a, int[] b) {
         //Write your code here
-        return Math.min(a.length, b.length);
+    	int[][] mat = new int[a.length + 1][b.length + 1];
+
+        for (int i = 1; i <= a.length; i++) {
+            for (int j = 1; j <= b.length; j++) {
+                if (a[i - 1] == b[j - 1]) {
+                    mat[i][j] = mat[i - 1][j - 1] + 1;
+                } else {
+                    mat[i][j] = Math.max(mat[i - 1][j], mat[i][j - 1]);
+                }
+            }
+        }
+        return mat[a.length][b.length];
     }
 
     public static void main(String[] args) {
