fed713715fd7693623a5d532f9b1f6bd00050fcb
ugaliguy <ugali_guy@hotmail.com>
2020-09-03 13:37:17 -0500

    Added code to optimal_sequence method

--- Algorithmic-Toolbox/week-5-Dynamic-Programming-1/PrimitiveCalculator.java
+++ Algorithmic-Toolbox/week-5-Dynamic-Programming-1/PrimitiveCalculator.java
@@ -3,15 +3,41 @@ import java.util.*;
 public class PrimitiveCalculator {
     private static List<Integer> optimal_sequence(int n) {
         List<Integer> sequence = new ArrayList<Integer>();
+//        while (n >= 1) {
+//            sequence.add(n);
+//            int n3 = n % 3;
+//            int n2 = n % 2;
+//            if (n % 3 == 0) {
+//                n /= 3;
+//            } else if (n % 2 == 0) {
+//                n /= 2;
+//            } else {
+//                n -= 1;
+//            }
+//        }
+        int[] minOperation = new int[n + 1];
+        int[] path = new int[n + 1];
+
+        for (int op = 2; op <= n; op++) {
+            int min = Integer.MAX_VALUE;
+            if (op % 3 == 0 && min > minOperation[op / 3] + 1) {
+                min = minOperation[op / 3] + 1;
+                path[op] = op / 3;
+            } 
+            if (op % 2 == 0 && min > minOperation[op / 2] + 1) {
+                min = minOperation[op / 2] + 1;
+                path[op] = op / 2;
+            }
+            if (min > minOperation[op - 1] + 1) {
+                min = minOperation[op - 1] + 1;
+                path[op] = op - 1;
+            }
+            minOperation[op] = min;
+        }
+
         while (n >= 1) {
             sequence.add(n);
-            if (n % 3 == 0) {
-                n /= 3;
-            } else if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n -= 1;
-            }
+            n = path[n];
         }
         Collections.reverse(sequence);
         return sequence;
