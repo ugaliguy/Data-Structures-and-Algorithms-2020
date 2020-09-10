8c1a7571dd2229f3475470b8295b7f59d490ac61
ugaliguy <ugali_guy@hotmail.com>
2020-09-09 20:26:47 -0500

    Removed commented-out old code - still not efficient

--- Course-2-Data-Structures/week-1-basic-data-structures/MaxSlidingWindow.java
+++ Course-2-Data-Structures/week-1-basic-data-structures/MaxSlidingWindow.java
@@ -3,121 +3,32 @@ import java.io.*;
 
 public class MaxSlidingWindow  {
 	
-//	public int[] maxSlidingWindow(int[] nums, int k) {
-//        int n = nums.length;
-//        int[] result = new int[n-k+1];
-//        int counter = 0;
-//        ArrayDeque<Integer> q = new ArrayDeque<>(); //notice that, this is not programmed to interface
-//        
-//        for(int i=0; i < k; i++) {
-//            //skip # 1 step 
-//            
-//             while(!q.isEmpty() && nums[i] >= nums[q.getLast()]) {
-//                 q.removeLast();
-//             }
-//            
-//            q.add(i);
-//        }
-//        
-//        result[counter++] = nums[q.peek()];
-//        
-//        for(int i=k; i < n; i++) {
-//            if(!q.isEmpty() && q.peek() < (i-k+1)) {
-//                q.remove();
-//            }
-//            
-//             while(!q.isEmpty() && nums[i] >= nums[q.getLast()]) {
-//                 q.removeLast();
-//             }
-//            
-//            q.add(i);
-//            
-//            result[counter++] = nums[q.peek()];
-//        }
-//        
-//        return result;
-//    }
-	
-//	public int[] maxSlidingWindow(int[] nums, int k) {
-//	    if(nums==null||nums.length==0)
-//	        return new int[0];
-//	 
-//	    int[] result = new int[nums.length-k+1];
-//	 
-//	    LinkedList<Integer> deque = new LinkedList<Integer>();
-//	    for(int i=0; i<nums.length; i++){
-//	        if(!deque.isEmpty()&&deque.peekFirst()==i-k) 
-//	            deque.poll();
-//	 
-//	        while(!deque.isEmpty()&&nums[deque.peekLast()]<nums[i]){
-//	            deque.removeLast();
-//	        }    
-//	 
-//	        deque.offer(i);
-//	 
-//	        if(i+1>=k)
-//	            result[i+1-k]=nums[deque.peek()];
-//	    }
-//	 
-//	    return result;
-//	}
-	
-//	private static int[] maxSlidingWindow(int[] nums, int k) {
-//        int ans[] = new int[nums.length - k + 1];
-//        TreeMap<Integer, Integer> map = new TreeMap<>();
-//        // Store the first k elements and their frequencies in a self balancing BST
-//        for (int i = 0; i < k; i++) {
-//            if (map.containsKey(nums[i])) {
-//                map.put(nums[i], map.get(nums[i]) + 1);
-//            } else {
-//                map.put(nums[i], 1);
-//            }
-//        }
-//        // Largest value of BST gives the first maximum
-//        ans[0] = map.lastKey();
-//        int index = 1;
-//        // Traverse the remaining elements
-//        for (int i = k; i < nums.length; i++) {
-//            // Remove first element of previous window from BST
-//            int freq = map.get(nums[i - k]);
-//            if (freq == 1) {
-//                map.remove(nums[i - k]);
-//            } else {
-//                map.put(nums[i - k], freq - 1);
-//            }
-//            
-//            // Add current element to BST
-//            if (map.containsKey(nums[i])) {
-//                map.put(nums[i], map.get(nums[i]) + 1);
-//            } else {
-//                map.put(nums[i], 1);
-//            }
-//            
-//            // Current asnwer is maximum value in BST
-//            ans[index++] = map.lastKey();
-//        }
-//        
-//        return ans;
-//	}
-	
-	private Deque<Integer> deque = new LinkedList<>();
-
-	public int[] maxSlidingWindow(int[] nums, int k) { int[] result = new int[nums.length-(k-1)]; int i = 0; for (i = 0; i < k;i++) { result[0] = maxBlackBox(nums,k, i); }
-
-	for (; i < nums.length; i++) {
-	  result[i - k + 1] = maxBlackBox(nums, k, i);
+	public class Solution {
+	    public int[] maxSlidingWindow(int[] nums, int k) {
+	        int n = nums.length;
+	        if (n == 0 || n == 1 || k == 1) {
+	            return nums;
+	        }
+	        int[] result = new int[n - k + 1];
+	        // Store index
+	        LinkedList<Integer> dq = new LinkedList<>();
+	        for (int i = 0; i < n; i++) {
+	            // remove index out of sliding window [i - k + 1, i]
+	            if (!dq.isEmpty() && dq.peek() < i - k + 1) {
+	                dq.poll();
+	            }
+	            // remove numbers in deque which are smaller than the new element nums[i] in the sliding window
+	            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+	                dq.pollLast();
+	            }
+	            dq.offer(i);
+	            if (i - k + 1 >= 0) {
+	                result[i - k + 1] = nums[dq.peek()];
+	            }
+	        }
+	        return result;
+	    }
 	}
-	return result;   }
-	private int maxBlackBox(int [] nums, int k, int i) { // Shart-2 while (!deque.isEmpty() && deque.peek() <= i - k) { deque.removeFirst(); }
-
-	// Shart-1
-	while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
-	  deque.removeLast();
-	}
-
-	deque.addLast(i);
-	return nums[deque.peek()];   
-	} 
 
     class FastScanner {
         StringTokenizer tok = new StringTokenizer("");
